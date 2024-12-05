9d0d12d05c1c0ec099a6ed149d76ee638b83acc3
Sime <jureskosime@gmail.com>
2024-12-05 13:34:21 +0100

    promjena

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TestSJ
+https://github.com/Sime11/TestSJ.git
+
+///SELECT SUM(broj_sati) AS ukupno_sati
+FROM evidencija 
+WHERE id = 1;
+///
+Sime Juresko
---
+++ TestSJ/src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card class="my-card" style="max-width:450px; width: 100%;">
+      <q-img src="~assets/quasar-logo-vertical.svg" alt="Quasar logo" style="height: 300px;" />
+      <q-card-section>
+        <div class="text-h6">TestSJ</div>
+        <div class="text-subtitle2">QCard</div>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
+
